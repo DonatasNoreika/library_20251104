f8cd485f27dc520d15262e42abdf5c9bca553bfc
DonatasNoreika <donoras@gmail.com>
2025-11-05 19:33:10 +0200

    2: +Aprašytas modelis Book, registruotas admin.py

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -13,3 +13,16 @@ class Author(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+
+class Book(models.Model):
+    title = models.CharField(verbose_name="Title")
+    summary = models.TextField(verbose_name="Summary")
+    isbn = models.CharField(verbose_name="ISBN", max_length=13)
+    author = models.ForeignKey(to="Author",
+                               verbose_name="Author",
+                               on_delete=models.SET_NULL,
+                               null=True, blank=True)
+    genre = models.ManyToManyField(to="Genre", verbose_name="Genre")
+
+    def __str__(self):
+        return self.title
