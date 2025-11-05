3569eaeceb1bf0f1aefcc06ee5dc3955c6b1b281
DonatasNoreika <donoras@gmail.com>
2025-11-05 18:58:15 +0200

    2: +Aprašytas modelis Genre, registruotas admin.py

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(verbose_name="Name")
+
+    def __str__(self):
+        return self.name
