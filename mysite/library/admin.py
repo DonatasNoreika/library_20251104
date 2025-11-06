dd096956fa8370da8c4b311abb86f2d1a585c5b5
DonatasNoreika <donoras@gmail.com>
2025-11-06 19:27:57 +0200

    3: +Prie egzempliorių įdėta paieška, redagavimas sąraše

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -21,6 +21,8 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ['uuid', 'book', 'due_back', 'status']
     list_filter = ['status', 'due_back', 'book']
+    search_fields = ['uuid', 'book__title']
+    list_editable = ['due_back', 'status']
     fieldsets = [
         ("General", {"fields": ('uuid', 'book')}),
         ("Availability", {"fields": ('status', 'due_back')}),
