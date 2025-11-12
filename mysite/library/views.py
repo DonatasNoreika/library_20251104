d211f8affe0f856e9cde22d6a6f9f7de4933bac4
DonatasNoreika <donoras@gmail.com>
2025-11-12 18:49:58 +0200

    6: +Autoriai dabar puslapiuojami

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book, BookInstance, Author
 from django.views import generic
+from django.core.paginator import Paginator
 
 def index(request):
     num_books = Book.objects.count()
@@ -17,8 +18,12 @@ def index(request):
 
 
 def authors(request):
+    authors = Author.objects.all()
+    paginator = Paginator(authors, per_page=3)
+    page_number = request.GET.get('page')
+    paged_authors = paginator.get_page(page_number)
     context = {
-        "authors": Author.objects.all()
+        "authors": paged_authors,
     }
     return render(request, template_name="authors.html", context=context)
 
