7100e209582f6768870dbaccfbd5e2517b58dc94
DonatasNoreika <donoras@gmail.com>
2025-11-18 19:14:38 +0200

    9: +Įdiegtos crispy formos. Ir pritaikytos registracijai

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'library',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -171,3 +173,6 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
     }
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
