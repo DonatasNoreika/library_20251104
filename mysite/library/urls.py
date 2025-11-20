2d8f482ce0d26f483d917dda4b4f42ce9654158d
DonatasNoreika <donoras@gmail.com>
2025-11-20 19:21:58 +0200

    11: +Paspaudus ant knygos, užeinama į jos aprašymą (DetailView). Jis prieinamas tik staff vartotojui

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     # path("profile/", views.ProfileUpdateView.as_view(), name="profile"),
     path("profile/", views.profile, name="profile"),
     path('instances/', views.BookInstanceListView.as_view(), name="instances"),
+    path('instances/<int:pk>/', views.BookInstanceDetailView.as_view(), name="instance"),
 ]
